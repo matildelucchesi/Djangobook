ed4458bf58e606723cfe850ed19e6cb6e6e31fd4
Matilde <matilde.lucchesi3@stud.unifi.it>
2023-05-28 00:28:51 +0200

    Fix some bugs

--- core/views.py
+++ core/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
-def singup(request):
-    return render(request, 'singup.html')
+def signup(request):
+    return render(request, 'signup.html')
