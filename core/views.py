7cfaf0393401331e7e0e538a1d13deb7f55d8370
Matilde <matilde.lucchesi3@stud.unifi.it>
2023-05-28 00:07:15 +0200

    Fix the name

--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def singup(request):
+    return render(request, 'singup.html')
