0712581f783f90f313cbcd9ce785bfc8d6db2988
Matilde <matilde.lucchesi3@stud.unifi.it>
2023-05-31 20:58:02 +0200

    Adding dropdown menu in settings icon but id doesn't work

--- core/views.py
+++ core/views.py
@@ -38,3 +38,20 @@ def signup(request):
         
     else:
         return render(request, 'signup.html')
+    
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+        
+    else:
+        return render(request, 'login.html')
--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -5,4 +5,37 @@ document.addEventListener('DOMContentLoaded', function() {
     const headerHeight = header.getBoundingClientRect().height;
 
     content.style.height = `${window.innerHeight - headerHeight}px`;
+
+});
+
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches('[dropdown-button]');
+    const d = document.querySelector('.dropdown-menu');
+    if (!isDropdownButton && e.target.closest('[dropdown]') != null) return;
+
+    let currentDropdown;
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[dropdown]');
+        currentDropdown.classList.toggle('active');
+        console.log('Dropdown:', currentDropdown, 'active:', currentDropdown.classList.contains('active'));
+
+        if (currentDropdown.classList.contains('active')) {
+            d.style.overflowY = 'auto';
+            d.style.opacity = '1';
+            document.body.style.overflowY = 'hidden';
+
+        } else {
+            d.style.overflowY = 'hidden';
+            document.body.style.overflowY = 'visible';
+            d.style.opacity = '0';
+        }
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return;
+        dropdown.classList.remove('active');
+        console.log('Dropdown:', dropdown, 'active:', dropdown.classList.contains('active'));
+        d.style.overflowY = 'hidden';
+        d.style.opacity = '0';
+    });
 });
