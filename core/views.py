530cfacc57052d5268dc9716aaaa04f5340e8cc3
Matilde <matilde.lucchesi3@stud.unifi.it>
2023-06-01 12:50:51 +0200

    Adding logout and fix some bugs

--- core/views.py
+++ core/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from core.models import Profile
 
 # Create your views here.
+
+@login_required(login_url='login')
+
 def index(request):
     return render(request, 'index.html')
 
@@ -55,3 +59,7 @@ def login(request):
         
     else:
         return render(request, 'login.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
