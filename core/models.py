25a392d998e1157696e2627f191d714e0e9fe01f
Matilde <matilde.lucchesi3@stud.unifi.it>
2023-06-04 22:13:42 +0200

    Fix some bugs relative to settings

--- core/models.py
+++ core/models.py
@@ -7,6 +7,8 @@ User = get_user_model()
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete= models.CASCADE)
     id_user = models.IntegerField()
+    first_name = models.CharField(max_length=100, default="")
+    second_name = models.CharField(max_length=100, default="")
     bio = models.TextField(blank = True)
     profile_img = models.ImageField(upload_to='profile_images', default = 'blank_propic.png')
     location = models.CharField(max_length=100, blank=True)
--- core/views.py
+++ core/views.py
@@ -35,7 +35,7 @@ def signup(request):
                 auth.login(request, user_login)
                 
                 user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id, first_name = "", second_name = "")
                 new_profile.save()
                 return redirect('settings')
         else:
