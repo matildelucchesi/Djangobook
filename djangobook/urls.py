9bc483a5539707874b9a2fc052abc2c61a9e88a5
Matilde <matilde.lucchesi3@stud.unifi.it>
2023-06-11 18:37:12 +0200

    Update urls.py

--- djangobook/urls.py
+++ djangobook/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('core.urls')),
-    path('signup', include('core.urls'))
 ]
 
 urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
