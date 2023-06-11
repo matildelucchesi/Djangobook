590039dc6988923e7429f58b962022f7912666a2
Matilde <matilde.lucchesi3@stud.unifi.it>
2023-06-11 22:04:35 +0200

    Update settings.py

--- djangobook/settings.py
+++ djangobook/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "sjango.middleware.security.SecurityMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
