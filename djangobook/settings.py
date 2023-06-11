c30fb0ba13e4a3b01e4097f370645f6b7edc41ab
Matilde <matilde.lucchesi3@stud.unifi.it>
2023-06-11 20:45:55 +0200

    Update settings.py

--- djangobook/settings.py
+++ djangobook/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "core"
+    "core",
+    
+    "django-social-share",
+    "taggit"
 ]
 
 MIDDLEWARE = [
